cdfa00aec3f5e7e380fbcdc98e5e1ffccfe634a0
Santosh Rawal <awesomekid.spa@gmail.com>
2023-05-30 11:42:40 +0545

    before data.txt

--- scoreboard.py
+++ scoreboard.py
@@ -5,7 +5,7 @@ class ScoreBoard(Turtle):
     def __init__(self):
         super().__init__()
         self.score = 0
-
+        self.high_score = 0
         self.color("white")
         self.penup()
         self.goto(0, 260)
@@ -13,11 +13,14 @@ class ScoreBoard(Turtle):
         self.update_scoreboard()
 
     def update_scoreboard(self):
-        self.write(f"Score ={self.score} ", align="center", font=("Arial", 24, "normal"))
+        self.clear()
+        self.write(f"Score ={self.score}    High Score = {self.high_score} ", align="center", font=("Arial", 24, "normal"))
 
-    def game_over(self):
-        self.goto(0, 0)
-        self.write("GAME OVER ", align="center", font=("Arial", 24, "normal"))
+    def reset(self):
+        if self.score > self.high_score:
+            self.high_score = self.score
+        self.score = 0
+        self.update_scoreboard()
 
     def increase_score(self):
         self.score += 1
